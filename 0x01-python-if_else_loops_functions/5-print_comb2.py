0ae4208e54164ebe61b2424d3eaf37339f68ff8f
theosero <sirfesto@gmail.com>
2022-12-07 11:09:28 -0800

    if, else, loops and functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:02d}".format(i), end="")
-    if i < 99:
-        print(", ", end="")
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02d}".format(i), end="")
