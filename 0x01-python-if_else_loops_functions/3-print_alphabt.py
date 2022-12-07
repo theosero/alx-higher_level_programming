da22a38ee46c803995b8d09d56a7f5ec2b6b4157
theosero <sirfesto@gmail.com>
2022-12-07 06:14:09 -0800

    if, else, loops and functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in 'abcdefghijklmnopqrstuvwxyz':
-    if i == 'e' and i == 'q':
+    if i == 'e' or i == 'q':
         continue
-    print("{}".format(i), end = '')
+    print("{}".format(i), end='')
