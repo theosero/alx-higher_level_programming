d35f850bc1e53af8af2a0d2bd5c231f9e8d5eb21
theosero <sirfesto@gmail.com>
2022-12-18 00:19:18 -0800

    command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == '__main__':
+
+    arg = len(sys.argv)
+
+    if arg == 0:
+        print("0 arguments.")
+    elif arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:")
+
+    for i in arg:
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
